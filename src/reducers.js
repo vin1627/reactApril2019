d2db902d5394893d3b0a0baf8904fc4f52a37d08
GBFvin <marvin@globalbrainforce.com>
2019-08-10 07:33:07 +0800

    initial commit develope

--- src/reducers.js
+++ src/reducers.js
@@ -7,6 +7,7 @@ const intialState = {
 export const searchRobots = (state=intialState, action={}) => {
   switch(action.type){
     case CHANGE_SEARCH_FIELD:
+      //console.log(state)
       return Object.assign({}, state, { searchField: action.payload });
     default:
       return state;
