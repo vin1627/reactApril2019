1d39c8b0f0782f3406c42fc536c3dc7b8417a263
GBFvin <marvin@globalbrainforce.com>
2019-08-10 07:40:55 +0800

    add cases

--- src/reducers.js
+++ src/reducers.js
@@ -5,10 +5,14 @@ const intialState = {
 }
 
 export const searchRobots = (state=intialState, action={}) => {
+  const CHANGE_SEARCH_FIELD_RESULT = "TEST"
   switch(action.type){
     case CHANGE_SEARCH_FIELD:
       //console.log(state)
       return Object.assign({}, state, { searchField: action.payload });
+    case CHANGE_SEARCH_FIELD_RESULT:
+          //console.log(state)
+    return Object.assign({}, state, { searchField: action.payload });
     default:
       return state;
   }
